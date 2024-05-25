42410bc539f7bd40ce881eb3ddf5742cabbe9908
Giovanna Paiva <gi.paiva.2005@gmail.com>
2024-05-25 02:11:23 -0300

    Começo do BotZob

---
+++ Bot/BotZob.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicTrick_Tirana.Bot
+{
+    internal class BotZob
+    {
+        Dictionary<string,JogadorInfo> jogadoresInfos;
+        string IdBot;
+
+        public BotZob(List<List<string[]>> cartas, string[] Id, string[] posicaoMesa, string IdBot)
+        {
+            for(int i = 0; i < Id.Length; i++)
+            {
+                jogadoresInfos.Add(Id[i], new JogadorInfo(Id[i], cartas[i]));
+            }
+
+            this.IdBot = IdBot;
+        }
+
+        public string Jogar()
+        {
+
+
+            string jogada = "";
+            return jogada;
+        }
+    }
+}
---
+++ Bot/JogadorInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicTrick_Tirana.Bot
+{
+    internal class JogadorInfo
+    {
+        public string IdJogador { get; set; }
+        public string PosicaoMesa { get; set; }
+        public List<string[]> Cartas { get; set; }
+
+        public JogadorInfo(string idJogador, List<string[]> cartas)
+        {
+            IdJogador = idJogador;
+            Cartas = cartas;
+        }
+    }
+}
